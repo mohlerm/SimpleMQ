b00b95d09f1c43e7354637204955cab279fac36c
Marcel Mohler <mohlerm@users.noreply.github.com>
2015-10-25 18:27:49 +0000

    tryout to fix weird performance

--- client/src/ch/mohlerm/trafficgen/TrafficGenerator.java
+++ client/src/ch/mohlerm/trafficgen/TrafficGenerator.java
@@ -57,8 +57,9 @@ public abstract class TrafficGenerator implements Runnable {
         messageCounter++;
         long start = System.nanoTime();
         // +2 because we start with 2 (0 is the register message, 1, the startTraffic , n+2 is the queue delete, n+3 is the endTraffic message)
-        while(System.nanoTime() - start < Config.CLIENTTIME*1E9 && (numberOfRequests == -1 || messageCounter < numberOfRequests+2 )) {
-            generateTraffic();
+        //while(System.nanoTime() - start < Config.CLIENTTIME*1E9 && (numberOfRequests == -1 || messageCounter < numberOfRequests+2 )) {
+        while((numberOfRequests == -1 || messageCounter < numberOfRequests+2 )) {
+        generateTraffic();
             messageCounter++;
             if(Config.CLIENTTHINKTIME > 0) {
                 // sleep for a predefined time
