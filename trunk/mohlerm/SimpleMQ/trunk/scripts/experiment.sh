375d983b8f8f474dad6c9d1edd26f419308de301
mohlerm <mohlerm@d0917403-a1de-4b70-9114-2abbe37d5020>
2015-10-26 08:09:45 +0000

    scripts

--- trunk/mohlerm/SimpleMQ/trunk/scripts/experiment.sh
+++ trunk/mohlerm/SimpleMQ/trunk/scripts/experiment.sh
@@ -396,5 +396,5 @@ rm $experimentId/server*
 #EOF
 
 echo "  Parse with python and generate graphs using matplotlib"
-screen -dmS python_experiment_$experimentId python3 graphs.py $clientCount $experimentId > $experimentId/experiment_$experimentId.txt
-screen -dmS python_experiment_server_$experimentId python3 graphs_server.py $clientCount $experimentId > $experimentId/experiment_server_$experimentId.txt
+screen -dmS python_experiment_$experimentId "python3 graphs.py $clientCount $experimentId > $experimentId/experiment_$experimentId.txt"
+screen -dmS python_experiment_server_$experimentId "python3 graphs_server.py $clientCount $experimentId > $experimentId/experiment_server_$experimentId.txt"
