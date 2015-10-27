459d39130c04824d4bf5c23ce17b60371ee9372d
Marcel Mohler <mohlerm@users.noreply.github.com>
2015-10-27 13:40:45 +0000

    more work on python and start script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,17 +1,17 @@
 dbMachine=52.28.191.89
 serverMachines=52.29.55.139,52.29.53.74
 clientMachines=52.29.53.244
-experimentConstant=longTrace
-i=30
-#for i in {10..30..5} #{5..20..5}
-#  do
-    bash experiment.sh --dbMachine=$dbMachine --dbPersistent=false --serverMachines=$serverMachines --serverWorkerTotal=64 --clientMachines=$clientMachines --clientTotal=$i --clientWorkload=staticsmall --clientRunTime=2000 --clientRunCount=-1 --remoteUserName=ubuntu --experimentId=$experimentConstant$i
-    ~/tg/bin/telegram-cli -k ~/tg/tg-server.pub -W -e "msg Marcel_Mohler Experiments $experimentConstant$i large done"
-#  done
-#serverMachines=52.29.55.139,52.29.53.74
-#experimentConstant=maxclientperinstance2srv_lrg
-#for i in {10..30..5} #{5..20..5}
-#  do
-#    bash experiment.sh --dbMachine=$dbMachine --dbPersistent=false --serverMachines=$serverMachines --serverWorkerTotal=64 --clientMachines=$clientMachines --clientTotal=$i --clientWorkload=staticlarge --clientRunTime=300 --clientRunCount=-1 --remoteUserName=ubuntu --experimentId=$experimentConstant$i
-#    ~/tg/bin/telegram-cli -k ~/tg/tg-server.pub -W -e "msg Marcel_Mohler Experiments $experimentConstant$i large done"
-#  done 
+experimentConstant=maxClientPerInstance
+declare -a workLoads={'staticsmall','staticlarge'}
+for workload in "${workLoads[@]}" # different workloads
+do
+  for clients in {10..30..5} # different clients
+  do
+    for rep in {1..5..1} # repetitions for statistical reasons
+    do
+      bash experiment.sh --dbMachine=$dbMachine --dbPersistent=false --serverMachines=$serverMachines --serverWorkerTotal=64 --clientMachines=$clientMachines --clientTotal=$clients --clientWorkload=$workload --clientRunTime=300 --clientRunCount=-1 --remoteUserName=ubuntu --experimentId=$experimentConstant-$workload-$clients-$rep
+    done
+    ~/tg/bin/telegram-cli -k ~/tg/tg-server.pub -W -e "msg Marcel_Mohler Experiments $experimentConstant-$workload-$clients 5 repetitions done"
+  done
+done
+   
