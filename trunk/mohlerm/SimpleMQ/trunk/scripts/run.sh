58c99b70f2241d8e852942bd8bd2fa1152dd3e16
mohlerm <mohlerm@d0917403-a1de-4b70-9114-2abbe37d5020>
2015-10-27 17:15:43 +0000

    updated eval script to include multiple measurements

--- trunk/mohlerm/SimpleMQ/trunk/scripts/run.sh
+++ trunk/mohlerm/SimpleMQ/trunk/scripts/run.sh
@@ -1,17 +1,17 @@
-dbMachine=52.28.191.89
-serverMachines=52.29.55.139,52.29.53.74
-clientMachines=52.29.53.244
+dbMachine=52.28.84.191
+serverMachines=52.28.43.155,52.28.181.169
+clientMachines=52.29.45.70
 experimentConstant=maxClientPerInstance
-declare -a workLoads={'staticsmall','staticlarge'}
+workLoads=('staticsmall' 'staticlarge')
 for workload in "${workLoads[@]}" # different workloads
 do
   for clients in {10..30..5} # different clients
   do
-    for rep in {1..5..1} # repetitions for statistical reasons
+    for rep in {1..3..1} # repetitions for statistical reasons
     do
       bash experiment.sh --dbMachine=$dbMachine --dbPersistent=false --serverMachines=$serverMachines --serverWorkerTotal=64 --clientMachines=$clientMachines --clientTotal=$clients --clientWorkload=$workload --clientRunTime=300 --clientRunCount=-1 --remoteUserName=ubuntu --experimentId=$experimentConstant-$workload-$clients-$rep
     done
-    ~/tg/bin/telegram-cli -k ~/tg/tg-server.pub -W -e "msg Marcel_Mohler Experiments $experimentConstant-$workload-$clients 5 repetitions done"
+    ~/tg/bin/telegram-cli -k ~/tg/tg-server.pub -W -e "msg Marcel_Mohler Experiments $experimentConstant-$workload-$clients 3 repetitions done"
   done
 done
    
