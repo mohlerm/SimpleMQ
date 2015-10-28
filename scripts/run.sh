72eb7f54a354d60af3d2b76e2dfc8f87ec55573e
Marcel Mohler <mohlerm@users.noreply.github.com>
2015-10-28 23:24:30 +0000

    update scripts etc

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,43 +1,19 @@
 dbMachine=52.29.31.58
-#serverMachines=52.29.30.57
-clientMachines=52.29.33.110,52.29.79.231
-#experimentConstant=maxClientPerInstance1srv
-#workLoads=('staticsmall' 'staticlarge')
-#for workload in "${workLoads[@]}" # different workloads
-#do
-#  for clients in {40..55..5} # different clients
-#  do
-#    for rep in {1..3..1} # repetitions for statistical reasons
-#    do
-#      bash experiment.sh --dbMachine=$dbMachine --dbPersistent=false --serverMachines=$serverMachines --serverWorkerTotal=64 --clientMachines=$clientMachines --clientTotal=$clients --clientWorkload=$workload --clientRunTime=300 --clientRunCount=-1 --remoteUserName=ubuntu --experimentId=$experimentConstant-$workload-$clients-$rep
-#    done
-#    ~/tg/bin/telegram-cli -k ~/tg/tg-server.pub -W -e "msg Marcel_Mohler Experiments $experimentConstant-$workload-$clients 3 repetitions done"
-#  done
-#done
 serverMachines=52.29.30.57
-experimentConstant=maxWorkerPerInstance
-workLoads=('staticsmall' 'staticlarge')
+clientMachines=52.29.33.110
+experimentConstant=maxThroughput
+workLoads=('staticsmall') # 'staticlarge')
 for workload in "${workLoads[@]}" # different workloads
 do
-  for workers in {10..100..10} # different workers
+  for clients in {60..180..20} # different clients
   do
-    for rep in {1..3..1} # repetitions for statistical reasons
+    for workers in {60..180..20}
     do
-      bash experiment.sh --dbMachine=$dbMachine --dbPersistent=false --serverMachines=$serverMachines --serverWorkerTotal=$workers --clientMachines=$clientMachines --clientTotal=60 --clientWorkload=$workload --clientRunTime=300 --clientRunCount=-1 --remoteUserName=ubuntu --experimentId=$experimentConstant-$workload-$workers-$rep
+      for rep in {1..3..1} # repetitions for statistical reasons
+      do
+        bash experiment.sh --dbMachine=$dbMachine --dbPersistent=false --serverMachines=$serverMachines --serverWorkerTotal=$workers --clientMachines=$clientMachines --clientTotal=$clients --clientWorkload=$workload --clientRunTime=300 --clientRunCount=-1 --remoteUserName=ubuntu --experimentId=$experimentConstant-$workload-$workers-$clients-$rep
+      done
     done
-    ~/tg/bin/telegram-cli -k ~/tg/tg-server.pub -W -e "msg Marcel_Mohler Experiments $experimentConstant-$workload-$workers 3 repetitions done"
+   # ~/tg/bin/telegram-cli -k ~/tg/tg-server.pub -W -e "msg Marcel_Mohler Experiments $experimentConstant-$workload-$clients 3 repetitions done"
   done
 done
-#experimentConstant=longRun30Min
-#workLoads=('staticsmall' 'staticlarge')
-#for workload in "${workLoads[@]}" # different workloads
-#do
-#  for clients in {30..30..5} # different clients
-#  do
-#    for rep in {1..3..1} # repetitions for statistical reasons
-#    do
-#      bash experiment.sh --dbMachine=$dbMachine --dbPersistent=false --serverMachines=$serverMachines --serverWorkerTotal=64 --clientMachines=$clientMachines --clientTotal=$clients --clientWorkload=$workload --clientRunTime=1980 --clientRunCount=-1 --remoteUserName=ubuntu --experimentId=$experimentConstant-$workload-$clients-$rep
-#    done
-#    ~/tg/bin/telegram-cli -k ~/tg/tg-server.pub -W -e "msg Marcel_Mohler Experiments $experimentConstant-$workload-$clients 3 repetitions done"
-#  done
-#done  
